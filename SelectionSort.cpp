1bd66f3b900797764cbe2e8bb263e84971782166
falafe1y <falafelyy@ya.ru>
2024-06-14 19:50:30 +0300

    First version

---
+++ SelectionSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void SelectionSort(vector<int> *arr, int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min = i;
+        for (int j = i+1; j < n; j++)
+        if ((*arr)[j] < (*arr)[min]) {
+            min = j;
+        }
+        swap((*arr)[min], (*arr)[i]);
+    }
+}
---
+++ SelectionSort.h
@@ -0,0 +1,8 @@
+#pragma once 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void SelectionSort(vector<int> *arr, int n);
